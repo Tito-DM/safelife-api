873af9c4594b729427105155e051a9d39e24e987
ariclinis <ariclinis1@gmail.com>
2021-09-08 14:01:38 +0100

    Password Reset

--- app/controllers/api/v1/PasswordResetsController.rb
+++ app/controllers/api/v1/PasswordResetsController.rb
@@ -2,6 +2,8 @@ class Api::V1::PasswordResetsController < DashboardController
     def new; end
     def edit
       # finds user with a valid token
+      token_decripted = Devise.token_generator.digest(User,:reset_password_token, params[:token])
+      @user = User.find_by!(reset_password_token: token_decripted)
       @user = User.find_signed!(params[:token], purpose: 'password_reset')
       rescue ActiveSupport::MessageVerifier::InvalidSignature
         json_response("O token está expirado, tente novamente.",false,{},@user,"Utilizador", :ok)
@@ -11,7 +13,7 @@ class Api::V1::PasswordResetsController < DashboardController
       user = User.find_by_email(params[:email])
       if user.present?
           user.send_reset_password_instructions
-          json_response("Foi enviado um email de recuperação para #{params[:email]}",true,{},user,"Utilizador", :created)
+          json_response("Foi enviado um email de recuperação para #{params[:email]}",true,{},user,"User", :created)
       else
           json_response("O email #{params[:email]} não exite.",false,{},user,"Utilizador", :created)
       end
@@ -19,7 +21,8 @@ class Api::V1::PasswordResetsController < DashboardController
 
     def update
         # updates user's password
-        @user = User.find_signed!(params[:token], purpose: 'password_reset')
+        token_decripted = Devise.token_generator.digest(User,:reset_password_token, params[:token])
+        @user = User.find_by!(reset_password_token: token_decripted)
         if @user.update(password_params)
             json_response("Palavra-passe alterada com sucesso.",true,{},@user,"Utilizador", :ok)
         else
