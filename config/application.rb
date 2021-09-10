9e84bc76e568267956d0c11ae3dd4f4aeeacc03d
ariclinis <ariclinis1@gmail.com>
2021-09-10 16:00:06 +0100

    cache redis

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module AppApi
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    config.active_record.cache_versioning = false
     config.cache_store = :redis_store, {
       host: "redis-13107.c78.eu-west-1-2.ec2.cloud.redislabs.com",
       port: 13107,
